37bb392805d035486f71683703a04d7cc2dc469a
Daniel Domański <hello@danieldomanski.com>
2020-07-07 13:44:05 +0200

    added vue, webpack, babel for basic project scaffolding

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <HelloWorld name="Hello world"></HelloWorld>
+  </div>
+</template>
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld
+  }
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Hello {{ name }}!</h1>
+</template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
